771d6e1d3896266ab04b1fcb1b34fac8ee8ae954
donaldshrimp <cets18024@g.nihon-u.ac.jp>
2019-09-04 13:41:11 +0900

    書きかけ

---
+++ confirm.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>GMGP</title>
+    <meta http-equiv="content-type" charset="utf-8">
+    <link rel="shortcut icon" href="favicon.ico" />
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgj\
+						WPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+
+<body>
+  <!--コンテナ-->
+  <div class="container">
+<!--マルチカラム範囲-->
+<div class="wrapper">
+  <div class="title"><img src="blackboard.png" alt="ロゴ画像" width="900"></div>
+  <div class="row">
+    <!--サイドバー-->
+   <aside class="sidebar">
+      <div id="side_menu" class="col-md-3 sticky">
+          <ul>
+            <li>ホーム</li>
+            <li>記事</li>
+            <li>概要</li>
+            <li>新規登録</li>
+            <li>過去問閲覧</li>
+            <li>おまけ</li>
+          </ul>
+      </div>
+    </aside>
+ <!--サイドバー終わり-->
+
+ <!--本文エリア-->
+ <section class="main-content">
+    <div id="main"　class="col-md-8 sticky">
+       <p>
+            確認<br>
+            <?php
+            echo "年次：" , $nenji ,"<br>";
+            echo "前後期：" ,if ($zenko==1) {
+
+            }
+            echo "教科名："
+            echo "中間・期末："
+            echo "年度："
+            アップロードするファイル：?>
+            こちらで間違いないですか？ <input type="submit" value="">
+       </p>
+    </div>
+  </section>
+     <!--本文エリア終わり-->
+
+   </div>
+   </div>
+   <!--マルチカラム範囲の終わり-->
+  </div>
+  <!--コンテナ終わり-->
+</body>
+</html>
--- upload.php
+++ upload.php
@@ -3,35 +3,45 @@ $dsn='mysql:dbname=kako_tag;host=http://13.112.20.136/phpmyadmin/server_database
 $user='root';
 $password='ZCVdqcanPHa5';
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <title>unti</title>
+    <title>GMGP</title>
     <meta http-equiv="content-type" charset="utf-8">
-    <link rel="shortcut icon" href="favicon.ico"/>
-    <style>#title{font-size:50px;position:relative;margin-left:30px;background-color:red}#side_menu{font-size:30px;color:red;width:20%;float:left;margin-left:30px;margin-right:100px;background-color:#7fffd4}#main{font-size:50px;padding-left:20px}.container{max-width:940px;margin:0 auto}</style>
+    <link rel="shortcut icon" href="favicon.ico" />
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgj\
+						WPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 
 </head>
+
 <body>
-
-
   <!--コンテナ-->
   <div class="container">
+<!--マルチカラム範囲-->
+<div class="wrapper">
+  <div class="title"><img src="blackboard.png" alt="ロゴ画像" width="900"></div>
+  <div class="row">
+    <!--サイドバー-->
+   <aside class="sidebar">
+      <div id="side_menu" class="col-md-3 sticky">
+          <ul>
+            <li>ホーム</li>
+            <li>記事</li>
+            <li>概要</li>
+            <li>新規登録</li>
+            <li>過去問閲覧</li>
+            <li>おまけ</li>
+          </ul>
+      </div>
+    </aside>
+ <!--サイドバー終わり-->
 
-    <div id="title">
-        <h1>情報研究会夏合宿</h1>
-    </div>
-<div class="row">
-    <div id="side_menu">
-        <ul>
-        <li>ファッキンコールド</li>
-        <li>ファッキンホット</li>
-        <li>ファッキン普通</li>
-        </ul>
-    </div>
-
-    <div id="main">
-        <form action="./confirm.php" method="post">
+ <!--本文エリア-->
+ <section class="main-content">
+    <div id="main"　class="col-md-8 sticky">
+    <form action="./confirm.php" method="post">
         <p>年次</p>
         <select name='nenji'>
         <option value='1'>1年</option>
@@ -91,6 +101,15 @@ $password='ZCVdqcanPHa5';
     </div>
 
     </div>
+  </div>
+  <!--コンテナ終わり-->
+    </div>
+  </section>
+     <!--本文エリア終わり-->
+
+   </div>
+   </div>
+   <!--マルチカラム範囲の終わり-->
   </div>
   <!--コンテナ終わり-->
 </body>
